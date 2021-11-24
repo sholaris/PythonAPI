49f11675088b0eae87c5afd58aa5db5ad68cc49c
Kamil Styś <kamil.stys1@gmail.com>
2021-11-24 19:06:30 +0100

    Delete user route added

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,4 +1,7 @@
 from fastapi import status, HTTPException, Depends, APIRouter
+from starlette.responses import Response
+
+from app import oauth2
 from ..database import get_db
 from sqlalchemy.orm import Session
 from .. import schemas, models, utils
@@ -25,3 +28,19 @@ def get_profile(id: int, db: Session = Depends(get_db)):
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {id} does not exist!")
     return user
+
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(id: int, db: Session = Depends(get_db), curr_user: int = Depends(oauth2.get_current_user)):
+    query = db.query(models.User).filter(models.User.id == id)
+    user = query.first()
+
+    if user == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with id {id} does not exist!')
+
+    if user.id != curr_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Not authorized to perform requested action')
+
+    query.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
